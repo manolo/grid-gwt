938c3e495757e22dc68493bb85019c259ee5241e
Manolo Carrasco <manolo@apache.org>
2015-08-20 15:44:22 +0200

    Adding more fields to Person

--- src/main/java/org/vaadin/artur/gridgwt/client/Person.java
+++ src/main/java/org/vaadin/artur/gridgwt/client/Person.java
@@ -6,16 +6,51 @@ import javax.validation.constraints.Size;
 
 class Person {
 
+
+    static int c = 0;
+
+    public final int id = c++;
+
     @Size(min = 5, max = 50)
     private String name;
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    private String surname;
+    private String job;
+    private int children;
 
     @Min(0)
     @Max(100)
     private int age;
 
-    public Person(String name, int age) {
+    public Person(String name, String surname, String job, int age, int children) {
         this.name = name;
+        this.surname = surname;
+        this.job = job;
         this.age = age;
+        this.children = children;
     }
 
     public String getName() {
