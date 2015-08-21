f3cb8a0ae8de5945be9eb9958fc4e85e1c89a09c
Manolo Carrasco <manolo@apache.org>
2015-08-21 09:14:24 +0200

    Check for the existence of a suitable element to add the grid

--- src/main/java/org/vaadin/artur/gridgwt/client/GridApp.java
+++ src/main/java/org/vaadin/artur/gridgwt/client/GridApp.java
@@ -1,11 +1,21 @@
 package org.vaadin.artur.gridgwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class GridApp implements EntryPoint {
     @Override
     public void onModuleLoad() {
-        RootPanel.get("grid").add(new MyGrid());
+        Panel p = RootPanel.get("new_grid");
+        if (p == null) {
+            p = RootPanel.get("grid");
+        }
+        if (p == null) {
+            Window.alert("Unable to find an element with new_grid or grid id");
+        } else {
+            p.add(new MyGrid());
+        }
     }
 }
